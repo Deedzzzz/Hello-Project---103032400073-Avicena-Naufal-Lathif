bce973d6f0c9597dc2fcfdde42e4d6e576bd0934
Defen21 <defenderramadham@gmail.com>
2025-10-29 23:46:18 +0700

    comment main :v

--- main.go
+++ main.go
@@ -1,16 +1,11 @@
 
-
-
-
-
-
 <!DOCTYPE html>
 <html
   lang="en"
   
   data-color-mode="auto" data-light-theme="light" data-dark-theme="dark"
   data-a11y-animated-images="system" data-a11y-link-underlines="true"
-  
+
   >
 
     <style>
@@ -21,7 +16,7 @@
 
 
 
-
+//hehe
   <head>
     <meta charset="utf-8">
   <link rel="dns-prefetch" href="https://github.githubassets.com">
